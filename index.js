cbe65b120d2349a24cc1147c68b8bfba433ef1b4
Hemang Patel <60358740+hemang-patel@users.noreply.github.com>
2020-03-17 13:51:46 -0400

    Another attempt

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const toolbox = require('./util/toolbox');
 
 let writer;
 let line = [];
-let accounts = [];
+let accountIds = [];
+let accountNames = [];
 let users = [];
 
-const NO_OF_TRANSACTIONS = 5000000;
+const NO_OF_TRANSACTIONS = 10000;
 const LINE_PER_TRANSACTION = 2;
-const NO_OF_ACCOUNTS = 100; // Max: 10000
-const NO_OF_USERS = 10; // Max: 5000
+const NO_OF_ACCOUNTS = 10000; // Max: 10000
+const NO_OF_USERS = 1; // Max: 5000
 const IS_MULTI_YEAR = false;
 
 const MIN_AMOUNT = -100000;
@@ -21,11 +22,12 @@ const FOLDER_NAME = 'performance-testing';
 
 class index {
     main() {
-        // toolbox.generateUsers(5000);
-        // toolbox.generateAccounts(10000);
+        // toolbox.generateUsers(15000);
+        // toolbox.generateAccounts(15000);
 
         // Store NO_OF_ACCOUNTS from accounts.csv (5k account id and name) to an array
-        this.fillAnArray('accounts');
+        this.fillAnArray('accountId');
+        this.fillAnArray('accountName');
         // Store NO_OF_ACCOUNTS from users.csv (5k user) to an array
         this.fillAnArray('users');
 
@@ -34,16 +36,22 @@ class index {
 
     fillAnArray(type) {
         switch (type) {
-            case 'accounts':
-                let totalAccounts = fs.readFileSync('resource/accounts.csv', 'utf-8').split(',');
-                for (let i = 0; i < NO_OF_ACCOUNTS - 1; i++) {
-                    accounts[i] = totalAccounts[Math.floor(Math.random() * totalAccounts.length) + 1].trim();
+            case 'accountId':
+                let totalAccId = fs.readFileSync('resource/accountId.csv', 'utf-8').split(',');
+                for (let i = 0; i < NO_OF_ACCOUNTS; i++) {
+                    accountIds[i] = totalAccId[i];
+                }
+                break;
+            case 'accountName':
+                let totalAccNames = fs.readFileSync('resource/accountName.csv', 'utf-8').split('\r');
+                for (let i = 0; i < NO_OF_ACCOUNTS; i++) {
+                    accountNames[i] = totalAccNames[i];
                 }
                 break;
             case 'users':
                 let totalUsers = fs.readFileSync('resource/users.csv', 'utf-8').split(',');
-                for (let i = 0; i < NO_OF_USERS - 1; i++) {
-                    users[i] = totalUsers[Math.floor(Math.random() * totalUsers.length) + 1].trim();
+                for (let i = 0; i < NO_OF_USERS; i++) {
+                    users[i] = totalUsers[i];
                 }
                 break;
         }
@@ -86,18 +94,19 @@ class index {
                     let detailComment = csvModel.getDetailComment();
                     let postingDate = csvModel.getPostingDate(IS_MULTI_YEAR);
                     let enteredDate = csvModel.getEnteredDate();
-                    let enteredBy = users[j];
+                    let enteredBy = _.sample(users);
+                    enteredBy = enteredBy.toString().trim();
                     let postingStatus = csvModel.getPostingStatus();
                     let entryApprovedBy = _.sample(users);
+                    entryApprovedBy = entryApprovedBy.toString().trim();
 
-                    let fullAccount = accounts[j];
-                    let accountID = fullAccount.toString().split(':')[0];
-                    let accountName = fullAccount.toString().split(':')[1];
-
+                    let accountID = accountIds[i].toString().trim();
+                    let accountName = accountNames[i].toString().trim();
                     let amount = csvModel.getAmount(MIN_AMOUNT, MAX_AMOUNT);
 
                     line = [entryID, entryNumber, documentType, detailComment, postingDate, enteredDate,
                         enteredBy, postingStatus, entryApprovedBy, accountID, accountName, amount];
+                    console.log(JSON.stringify(line));
                     process.stdout.write(`Creating Transaction ${i + 1}, line ${j + 1} ` + '\r');
                     writer.write(line);
                 }
--- util/toolbox.js
+++ util/toolbox.js
@@ -1,3 +1,4 @@
+"use strict";
 let moment = require('moment');
 const fs = require('fs');
 const csvModel = require('../engine/csv-model');
@@ -22,17 +23,29 @@ module.exports.generateUsers = function (number = 5000) {
     console.log(`Generated ${number} Users in ${filePath}`);
 };
 
-module.exports.generateAccounts = function (number = 5000) {
-    let filePath = `${process.cwd()}/resource/accounts.csv`;
-    if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath);
-        console.log(`Deleted accounts.csv file `);
+module.exports.generateAccounts = function (number = 10000) {
+   /* let accIdFile = `${process.cwd()}/resource/accountId.csv`;
+    if (fs.existsSync(accIdFile)) {
+        fs.unlinkSync(accIdFile);
+        console.log(`Deleted accountId.csv file `);
     }
     for (let i = 0; i < number; i++) {
         let accountId = csvModel.getAccountID();
-        let accountName = csvModel.getAccountName();
-        let accString = `${accountId}:${accountName},`;
-        fs.writeFileSync(`${process.cwd()}/resource/accounts.csv`, `${accString}` + '\n', {flag: 'a'});
+        // let accountName = csvModel.getAccountName();
+        let accString = `${accountId}`;
+        fs.writeFileSync(`${accIdFile}`, `${accString},` + '\n', {flag: 'a'});
     }
-    console.log(`Generated ${number} Accounts in ${filePath}`);
+    console.log(`Generated ${number} Account ids in ${accIdFile}`);*/
+
+    let accNameFile = `${process.cwd()}/resource/accountName.csv`;
+    if (fs.existsSync(accNameFile)) {
+        fs.unlinkSync(accNameFile);
+        console.log(`Deleted accountName.csv file `);
+    }
+    for (let i = 0; i < number; i++) {
+        let accountName = csvModel.getAccountName();
+        let accString = `${accountName}`;
+        fs.writeFileSync(`${accNameFile}`, `${accString},` + '\n', {flag: 'a'});
+    }
+    console.log(`Generated ${number} Account names in ${accNameFile}`);
 };
